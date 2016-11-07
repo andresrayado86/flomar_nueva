7e57921cb92a5424e222ee4f6f440f0343e601af
Andres Alberto Aguilar Rodriguez <andres_aguilar_86@hotmail.com>
2016-11-07 15:46:19 -0600

    Se crea la carpeta de fonts
    Se agrega la letra del logotipo

---
+++ views/index.view.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+		<title>Flomar Materiales y Aceros</title>
+		<link rel="shortcut icon" href="imagenes/flom.ico">
+		<link rel="stylesheet" href="font-awesome-4.6.3/css/font-awesome.min.css">
+		<link href="https://fonts.googleapis.com/css?family=Archivo+Narrow" rel="stylesheet">
+		<link rel="stylesheet" href="css/estilos.css">
+	</head>
+	<body>
+		<nav class="nav-main">
+			<div class="logo"><a href="#" class="navbar-brand"><img src="imagenes/flomarV3320.png" style="margin-top:-15px;"></a></div>
+			<ul>
+				<li><a href="#" class="nav-item">Inicio</a></li>
+				<li><a href="#" class="nav-item">Quienes Somos</a></li>
+				<li><a href="#" class="nav-item">Productos</a></li>
+				<li><a href="#" class="nav-item">Ubicación</a></li>
+				<li><a href="#" class="nav-item">Productos</a></li>
+			</ul>
+		</nav>
+	</body>
+</html>
