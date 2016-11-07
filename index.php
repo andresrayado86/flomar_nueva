3cc6fc51ba8068c4092e12535f6f3072f75ef763
andresrayado86 <andres_aguilar_86@hotmail.com>
2016-11-06 19:49:59 -0600

    se inicia el documento de estilos y el index se agregan las imagenes a la carpeta de imagenes

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Flomar Materiales y Aceros</title>
+		<link rel="shortcut icon" href="imagenes/flom.ico">
+		<link rel="stylesheet" href="font-awesome-4.6.3/css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/estilos.css">
+	</head>
+	<body>
+		<div class="contenedor">
+			<h2>Encabezado</h2>
+			
+		</div>
+	</body>
+</html>
